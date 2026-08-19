a2369db96be3a0cb6003609918fe6b9508e60c6a
Nityam Tiwari <nityamtiwari08@gmail.com>
2026-08-20 01:05:42 +0530

    Version updated for local development

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 20260822
-        versionName = "1.4"
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner" }
 
